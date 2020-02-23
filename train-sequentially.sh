2cf860efc7310bd4d60341ac4d71ed09a0234893
Guoliang Cao <gcao99@gmail.com>
2020-02-23 09:28:38 -0500

    Automate training sequentially

---
+++ train-sequentially.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -eu
+
+dir=$(pwd)
+
+round=1
+
+while true
+do
+    echo ==== Round $round start ====
+    echo
+    start=$SECONDS
+
+    echo ==== Round $round self-play ====
+    echo
+    cpp/katago selfplay -output-dir shared/selfplay -models-dir shared/models -config-file cpp/configs/selfplay1.cfg
+
+    echo ==== Round $round shuffle and export ====
+    echo
+    cd python
+    ./selfplay/shuffle_and_export_loop.sh CAO ../shared/ ../shared/scratch 4 1
+
+    echo ==== Round $round train ====
+    echo
+    ./selfplay/train.sh ../shared/ CAO b6c96 main -lr-scale 1.0
+
+    echo ==== Round $round gatekeeper ====
+    echo
+    cpp/katago gatekeeper -rejected-models-dir shared/rejectedmodels -accepted-models-dir shared/models/ -sgf-output-dir shared/gatekeepersgf/ -test-models-dir shared/modelstobetested/ -config-file cpp/configs/gatekeeper1.cfg
+
+    echo ==== Round $round end ====
+    seconds = $((SECONDS - start))
+    printf "Time: %02d:%02d:%02d" $(((seconds / 3600))) $(((SECONDS % 3600) / 60)) $((seconds % 60))
+    echo
+
+    cd ..
+    sleep 10
+
+    round=$((round + 1))
+done
