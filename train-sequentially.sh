79de895124e245002b3f9c14841b577364c4e63b
Guoliang Cao <gcao99@gmail.com>
2020-03-17 09:18:33 -0400

    Self play: 64 games, gatekeeper: 16 games, train-sequentially all in one

--- train-sequentially.sh
+++ train-sequentially.sh
@@ -17,22 +17,22 @@ do
     echo ==== Round $round shuffle and export ====
     echo
     cd python
-    ./selfplay/shuffle_and_export_loop.sh CAO ../shared/ ../shared/tmp 4 0
+    ./selfplay/shuffle_and_export_loop.sh CAO ../shared/ ../shared/tmp 4 1
 
     echo ==== Round $round train ====
     echo
     ./selfplay/train.sh ../shared/ CAO b6c96 main -lr-scale 1.0
     cd ..
 
-    # echo ==== Round $round gatekeeper ====
-    # echo
-    # cpp/katago gatekeeper \
-    #   -rejected-models-dir shared/rejectedmodels/ \
-    #   -accepted-models-dir shared/models/ \
-    #   -sgf-output-dir shared/gatekeepersgf/ \
-    #   -test-models-dir shared/modelstobetested/ \
-    #   -config-file cpp/configs/gatekeeper1.cfg \
-    #   -quit-if-no-nets-to-test 1
+    echo ==== Round $round gatekeeper ====
+    echo
+    cpp/katago gatekeeper \
+      -rejected-models-dir shared/rejectedmodels/ \
+      -accepted-models-dir shared/models/ \
+      -sgf-output-dir shared/gatekeepersgf/ \
+      -test-models-dir shared/modelstobetested/ \
+      -config-file cpp/configs/gatekeeper1.cfg \
+      -quit-if-no-nets-to-test 1
 
     echo ==== Round $round end ====
     seconds=$((SECONDS - start))
