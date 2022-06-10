3aaf37c5e3b844ca08b11fa949a58eef49f6697d
Guoliang Cao <gcao99@gmail.com>
2022-06-10 19:06:53 -0400

    Undo minor changes

--- gatekeeper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-cpp/katago gatekeeper -rejected-models-dir shared/rejectedmodels -accepted-models-dir shared/models -sgf-output-dir shared/gatekeepersgf -test-models-dir shared/modelstobetested -config-file cpp/configs/gatekeeper1.cfg
