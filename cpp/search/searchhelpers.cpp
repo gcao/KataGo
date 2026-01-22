1ff79035a0dce1c3422e6e22ae8558e66b80cf52
Guoliang Cao <gcao99@gmail.com>
2026-01-22 10:53:32 -0500

    Add Daoqi wall wrap-around checks to searchhelpers.cpp and nninputs.cpp

--- cpp/search/searchhelpers.cpp
+++ cpp/search/searchhelpers.cpp
@@ -497,6 +497,7 @@ bool Search::shouldSuppressPass(const SearchNode* n) const {
     bool adjToPlaOwned = false;
     for(int j = 0; j<4; j++) {
       Loc adj = moveLoc + rootBoard.adj_offsets[j];
+      if(rootBoard.colors[adj] == C_WALL) adj = moveLoc + rootBoard.adj_offsets[j + 4];
       if(rootBoard.isOnBoard(adj)) {
         int adjPos = NNPos::locToPos(adj,rootBoard.x_size,nnXLen,nnYLen);
         double adjPlaOwnership = rootPla == P_WHITE ? whiteOwnerMap[adjPos] : -whiteOwnerMap[adjPos];
